3c103465de82f22d2de6484756b548f84e748766
John <ionut_popovici92@yahoo.com>
2017-09-01 16:35:51 +0300

    Cleanup commit. Current design pattern used : DELEGATION.

--- StackApp/StackApp/QuestionViewController.swift
+++ StackApp/StackApp/QuestionViewController.swift
@@ -8,44 +8,27 @@
 
 import UIKit
 
-class QuestionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, StackManagerDelegate{
+class QuestionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, StackManagerDelegate {
     
-    let stackManager = StackManager()
-    // let listQuestions: [Question] = []
-//    var listOfQuestions = [Question]()
-    
+    let stackManager = StackManagerDLG()
     @IBOutlet weak var table: UITableView!
     
-    // let list = ["Milk", "Honey", "Bread", "Tacos", "Tomatoes"]
-    
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        print("<---------- listOfQuestions.count = \(listOfQuestions.count)")
-//        return listOfQuestions.count
         return self.stackManager.arrayOfQuestions.count
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ViewControllerTableViewCell
-//        let cellQuestion = listOfQuestions[indexPath.row]
         let questionArray = self.stackManager.arrayOfQuestions
         let cellQuestion = questionArray[indexPath.row]
-//        cell.textLabel?.text = cellQuestion.title
         cell.questionLabel.text = cellQuestion.title
         
         let dateOfQuestion = Date(timeIntervalSince1970: cellQuestion.last_activity_date!)
         
         let dateFormatter = DateFormatter()
-        // dateFormatter.timeStyle = DateFormatter.Style.medium // Set time style
-        // dateFormatter.dateStyle = DateFormatter.Style.medium // Set date style
-//        dateFormatter.locale = Locale(identifier: "en_GB")
-//        dateFormatter.setLocalizedDateFormatFromTemplate("MMMMddYYYY, hh:mm")
         dateFormatter.dateFormat = "dd MMMM YYYY, HH:mm"
-        
         let localDate = dateFormatter.string(from: dateOfQuestion as Date)
-        
         cell.dateLabel.text = localDate
-        
-        print("<---------- listOfQuestions[indexPath.row] = \(indexPath.row)")
         return cell
     }
 
@@ -65,10 +48,4 @@ class QuestionViewController: UIViewController, UITableViewDelegate, UITableView
     func dataTaskHasCompleted() {
         self.table.reloadData()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
--- StackApp/StackApp/StackManagerDLG.swift
+++ StackApp/StackApp/StackManagerDLG.swift
@@ -1,5 +1,5 @@
 //
-//  StackManager.swift
+//  StackManagerDLG.swift
 //  StackApp
 //
 //  Created by John on 8/24/17.
@@ -12,7 +12,7 @@ protocol StackManagerDelegate {
     func dataTaskHasCompleted()
 }
 
-class StackManager {
+class StackManagerDLG {
     
     var delegate: StackManagerDelegate?
     
@@ -32,18 +32,12 @@ class StackManager {
                         let questionsDictionary = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as? Dictionary<String, Any>
                         if let unwrappedQuestionsDictionary = questionsDictionary {
                             let questions: Array<Dictionary<String, Any>> = unwrappedQuestionsDictionary["items"] as! Array<Dictionary<String, Any>>
-                            print("**********")
-                            print("Show questions!")
-                            print(questions)
-                            print("End Show questions!")
-//                            print("Show Title, Last activity date, Question ID")
-                            
+
                             for question in questions {
                                 var currentQuestion = Question()
                                 currentQuestion.title = question["title"] as? String
                                 currentQuestion.last_activity_date = question["last_activity_date"] as? Double
                                 currentQuestion.question_id = question["question_id"] as? Double
-                                print("*** currentQuestion:")
                                 print(currentQuestion)
                                 
                                 self?.arrayOfQuestions.append(currentQuestion)
@@ -53,28 +47,13 @@ class StackManager {
                                 self?.delegate?.dataTaskHasCompleted()
                             }
 
-                            print()
-                            print("*** arrayOfQuestions:")
-                            print(self?.arrayOfQuestions)
-                            print()
-                            print("*** arrayOfQuestions: question no. 1:")
-                            print(self?.arrayOfQuestions[0])
-                            print("*** arrayOfQuestions: question no. 1: Formated date:")
-                            print(self?.arrayOfQuestions[0].last_activity_date)
-                            print()
-                            print("My [Question] was constructed. (network request finished + parsing done)")
-                        } // end if
-                        
-                        // print(questionsDictionary)
+                        }
                     }
                      
                     catch {}
-//                    print(content)
                 }
-//                print("**********")
         }
         
         sessionDataTask.resume()
     }
-    
 }
