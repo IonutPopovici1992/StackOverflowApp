099898187da5c399ddcc8e77e0d89335a79ec2b6
John <ionut_popovici92@yahoo.com>
2017-09-14 16:07:21 +0300

    Added a new loadAnswers() class method to StackManagerClosures. It uses Alamofire and ObjectMapper in order to make requests to the stack overflow api.

--- StackApp/StackApp/StackManagerClosures.swift
+++ StackApp/StackApp/StackManagerClosures.swift
@@ -7,7 +7,38 @@
 //
 
 import Foundation
+import Alamofire
+import AlamofireObjectMapper
+//import ObjectMapper
 
 class StackManagerClosures {
     
+    private static let baseStackOverflowURLString = "https://api.stackexchange.com/2.2/"
+    private static let filterID = "!b0OfNenVjyled*"
+    private static let maximumPageSize = 100
+    
+    class func loadAnswers(forQuestion: Question,
+                           pageIndex: Int,
+                           pageSize: Int,
+                           completionHandler: @escaping (DataResponse<[Answer]>) -> Void) {
+        
+        let filter = StackManagerClosures.filterID
+        let baseURLString = StackManagerClosures.baseStackOverflowURLString
+        let maxPageSize = StackManagerClosures.maximumPageSize
+        
+        // The API has a 100 limit for the pagesize argument.
+        let limitedPageSize = pageSize > maxPageSize ? maxPageSize : pageSize
+        
+        let answersAPI = baseURLString + "answers/\(String(describing: forQuestion.question_id))?page=\(pageIndex)&pagesize=\(limitedPageSize)&order=desc&sort=activity&site=stackoverflow&filter=\(filter)"
+        let url = URL(string:answersAPI)!
+        
+        let dataRequest = Alamofire.request(url,
+                                            method: HTTPMethod.get,
+                                            parameters: nil,
+                                            encoding: JSONEncoding.default,
+                                            headers: nil)
+        dataRequest.responseArray { (response: DataResponse<[Answer]>) in
+            completionHandler(response)
+        }
+    }
 }
