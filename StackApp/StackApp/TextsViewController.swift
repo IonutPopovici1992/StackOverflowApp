c350d3eee0276bc15710487985ded4fafd8bb0a4
John <ionut_popovici92@yahoo.com>
2017-12-15 12:35:08 +0200

    Modified TextsViewController

--- StackApp/StackApp/TextsViewController.swift
+++ StackApp/StackApp/TextsViewController.swift
@@ -22,29 +22,14 @@ class TextsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         configButtons()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     fileprivate func configButtons() {
         self.button.layer.borderColor = UIColor.black.cgColor;
         self.button.layer.borderWidth = 1;
         self.button.layer.cornerRadius = 5;
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
